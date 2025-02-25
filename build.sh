4a166f4a7a07c1050c5db4b807ace91c601de7a8
Chris Moorhouse <chris@mobiusflow.com>
2025-02-25 10:16:19 +0000

    Add mobius_only and mobius_with_view

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ sed -i -e 's/balena_up_mobiusflow-v2-le.*/balena_up_mobiusflow-v2-le:'$buildver\
 sed -i -e 's/balena_up_manage-v2.*/balena_up_manage-v2:'$buildver'/g' ./docker-compose.yml
 rm -f ./docker-compose.yml-e
 
-cd apps/mobius_with_view
+cd ../../apps/mobius_with_view
 sed -i -e 's/version: .*/version: \"'$buildver'\"/g' ./balena.yml
 rm -f ./balena.yml-e
 sed -i -e 's/balena_up_mobiusflow-v2-le.*/balena_up_mobiusflow-v2-le:'$buildver\\_$corever'/g' ./docker-compose.yml
